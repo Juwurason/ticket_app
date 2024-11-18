55d843911025bb6f9715db8b43b2b7f242f0eb09
Juwurason <ajibolasunday7@gmail.com>
2024-11-18 11:18:22 +0100

    profile page started

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -20,12 +20,13 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-     backgroundColor: AppStyles.bgColor,
-      body:ListView(
+      backgroundColor: AppStyles.bgColor,
+      body: ListView(
         children: [
-          const SizedBox(height: 40,),
+          const SizedBox(
+            height: 40,
+          ),
           Container(
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Column(
@@ -37,69 +38,102 @@ class _HomeScreenState extends State<HomeScreen> {
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("Good morning", style: AppStyles.headLineStyle3,),
-                        const SizedBox(height: 5,),
-                        Text("Book Tickets",  style: AppStyles.headLineStyle1,)
+                        Text(
+                          "Good morning",
+                          style: AppStyles.headLineStyle3,
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          "Book Tickets",
+                          style: AppStyles.headLineStyle1,
+                        )
                       ],
                     ),
                     Container(
                       width: 50,
                       height: 50,
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                       image: const DecorationImage(image: AssetImage(AppMedia.logo))
-                      ),
+                          borderRadius: BorderRadius.circular(10),
+                          image: const DecorationImage(
+                              image: AssetImage(AppMedia.logo))),
                     )
                   ],
                 ),
                 const SizedBox(height: 25),
                 Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
                   decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: const Color(0xFFF4F6FD),
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFFF4F6FD),
                   ),
                   child: const Row(
                     // mainAxisAlignment:  MainAxisAlignment.spaceBetween,
                     children: [
-                      Icon(FluentSystemIcons.ic_fluent_search_regular, color: Color(0xFFBFC205),),
-                  Text("Search"),
+                      Icon(
+                        FluentSystemIcons.ic_fluent_search_regular,
+                        color: Color(0xFFBFC205),
+                      ),
+                      Text("Search"),
                     ],
                   ),
                 ),
                 const SizedBox(height: 40),
-                AppDoubleText(bigTest: 'Upcoming Flights', smallTest: 'View all',
-                func: () => Navigator.pushNamed(context, AppRoutes.allTickets),
+                AppDoubleText(
+                  bigTest: 'Upcoming Flights',
+                  smallTest: 'View all',
+                  func: () =>
+                      Navigator.pushNamed(context, AppRoutes.allTickets),
                 ),
                 const SizedBox(height: 20),
                 SingleChildScrollView(
                   scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: ticketList.take(2).map((singleTicket) =>
-                      TicketView(ticket:singleTicket)
-                      ).toList(),
-                    ),
+                  child: Row(
+                    children: ticketList
+                        .take(2)
+                        .map((singleTicket) => GestureDetector(
+                            onTap: () {
+                              var index = ticketList.indexOf(singleTicket);
+
+                              Navigator.pushNamed(
+                                  context, AppRoutes.ticketScreen,
+                                  arguments: {"index": index});
+                            },
+                            child: TicketView(ticket: singleTicket)))
+                        .toList(),
+                  ),
                 ),
                 const SizedBox(height: 40),
-                 AppDoubleText(bigTest: 'Hotels', smallTest: 'View all',
-                     func: (){
-                       Navigator.pushNamed(context, AppRoutes.allHotels);
-                     },
-                 ),
+                AppDoubleText(
+                  bigTest: 'Hotels',
+                  smallTest: 'View all',
+                  func: () {
+                    Navigator.pushNamed(context, AppRoutes.allHotels);
+                  },
+                ),
                 const SizedBox(height: 20),
                 SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
+                    scrollDirection: Axis.horizontal,
                     child: Row(
-                      children: hotelList.take(2).map((singleHotel) =>
-                          Hotel(hotel: singleHotel)
-                      ).toList(),
-                    )
-                )
+                      children: hotelList
+                          .take(2)
+                          .map((singleHotel) =>
+                              GestureDetector(
+                                onTap: (){
+                                  var index = hotelList.indexOf(singleHotel);
 
+                                  Navigator.pushNamed(context, AppRoutes.hotelDetail, arguments: {
+                                    "index": index
+                                  });
+                                },
+                                  child: Hotel(hotel: singleHotel)))
+                          .toList(),
+                    ))
               ],
             ),
           ),
-
         ],
       ),
     );
